67a86783409eff3081770f1fc10e8964ec676ff8
originalkyu <originalkyu@gmail.com>
2023-07-24 11:27:59 +0900

    07.24

---
+++ webclientdemo/src/test/java/kyu/webclientdemo/WebclientdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package kyu.webclientdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebclientdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
